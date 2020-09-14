b6fd63554c665c076ecd6cc5c06fd3e558883410
Oliver <oliek365@student.liu.se>
2020-09-14 11:02:43 +0200

    Created project, Created controller with actions that query a mysql database

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using MySql.Data.MySqlClient;
+using SeriousAPI.Models;
+
+namespace SeriousAPI.Controllers
+{
+    [Route("api/Products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private MySqlDatabase MySqlDatabase { get; set; }
+        public ProductsController(MySqlDatabase mySqlDatabase)
+        {
+            this.MySqlDatabase = mySqlDatabase;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> ListProducts()
+        {
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM ProductsTable";
+            
+            MySqlDataReader dataReader = await Task.Run(() => cmd.ExecuteReader());
+
+            return CreateProductList(dataReader);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+
+            cmd.CommandText = @"INSERT INTO ProductsTable(ProductName, ProductPrice, ProductStock, ProductDescription)" +
+                "VALUES(@Name, @Price, @Stock, @Description);";
+            cmd.Parameters.AddWithValue("@Name", product.Name);
+            cmd.Parameters.AddWithValue("@Price", product.Price);
+            cmd.Parameters.AddWithValue("@Stock", product.Stock);
+            cmd.Parameters.AddWithValue("@Description", product.Description);
+
+            await Task.Run(() => cmd.ExecuteNonQuery());
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteProductById(int id)
+        {
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+
+            cmd.CommandText = @"DELETE FROM ProductsTable WHERE ProductId = @Id;";
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            await Task.Run(() => cmd.ExecuteNonQuery());
+            return NoContent();
+        }
+
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteProductByName(string name)
+        {
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+
+            cmd.CommandText = @"DELETE FROM ProductsTable WHERE ProductName = @Name;";
+            cmd.Parameters.AddWithValue("@Name", name);
+
+            await Task.Run(() => cmd.ExecuteNonQuery());
+            return NoContent();
+        }
+
+        private List<Product> CreateProductList(MySqlDataReader dataReader)
+        {
+            List<Product> products = new List<Product>();
+
+            while (dataReader.Read())
+            {
+                Product product = new Product();
+                product.Id = (int)dataReader["ProductId"];
+                product.Name = (string)dataReader["ProductName"];
+                product.Price = (int)dataReader["ProductPrice"];
+                product.Stock = (int)dataReader["ProductStock"];
+                product.Name = (string)dataReader["ProductName"];
+                if (dataReader["ProductDescription"].GetType() != typeof(DBNull))
+                {
+                    product.Description = (string)dataReader["ProductDescription"];
+                } else
+                {
+                    product.Description = "";
+                }
+
+                products.Add(product);
+            }
+
+            return products;
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriousAPI.Models 
+{
+    public class Product : IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Price { get; set; } //try feeding negative value
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        public string Description { get; set; }
+        /*[Required]
+         * public imagedatatype Image { get; set; }
+         */
+
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errorResponseList = new List<ValidationResult>();
+            if (Name.Length < 1)
+            {
+                errorResponseList.Add(new ValidationResult("Name has to contain at least one character"));
+            } else if (Name.Length > 120)
+            {
+                errorResponseList.Add(new ValidationResult("Name can not contain more than 120 characters"));
+            }
+            return errorResponseList;
+        }
+    }
+}
